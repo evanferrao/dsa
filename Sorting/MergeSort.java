42bce32bf4a4351e501601148c19aa40390b3148
Evan Ferrao <evanferrao@gmail.com>
2024-08-11 23:26:54 +0530

    clean unused code

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -35,54 +35,7 @@ public class MergeSort{
             merge(arr, beginning, mid, end);
         }
     }
-    // static void merge(int arr[], int beginning, int mid, int end){
-    //     // find the size of two subarrays to be merged
-    //     int n1 = mid - beginning + 1;
-    //     int n2 = end - mid;
-
-    //     // create temp arrays
-    //     int L[] = new int[n1];
-    //     int R[] = new int[n2];
-
-    //     // copy data to temp arrays
-    //     for(int i = 0; i < n1; i++){
-    //         L[i] = arr[beginning + i];
-    //     }
-    //     for(int j = 0; j < n2; j++){
-    //         R[j] = arr[mid + 1 + j];
-    //     }
-
-    //     // merge the temp arrays
-    //     // initial indexes of first and second subarrays
-    //     int i = 0, j = 0;
-
-    //     // initial index of merged subarray array
-    //     int k = beginning;
-    //     while(i < n1 && j < n2){
-    //         if(L[i] <= R[j]){
-    //             arr[k] = L[i];
-    //             i++;
-    //         }else{
-    //             arr[k] = R[j];
-    //             j++;
-    //         }
-    //         k++;
-    //     }
-
-    //     // copy remaining elements of L[] if any
-    //     while(i < n1){
-    //         arr[k] = L[i];
-    //         i++;
-    //         k++;
-    //     }
-
-    //     // copy remaining elements of R[] if any
-    //     while(j < n2){
-    //         arr[k] = R[j];
-    //         j++;
-    //         k++;
-    //     }
-    // }
+  
     static void merge(int[] arr, int low, int mid, int high) {
         ArrayList<Integer> temp = new ArrayList<>(); // temporary array
         int left = low;      // starting index of left half of arr
